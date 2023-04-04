764bc2dfae0a22b4dde4898ee96cf7c2fcd419c9
Memorizu <kaidohmary@gmail.com>
2023-04-04 21:51:33 +0300

    create class Request

---
+++ entitys/bot.py
@@ -0,0 +1,9 @@
+import telebot
+
+
+bot = telebot.TeleBot('6252209212:AAFW8AnZirrhTBi-4xveFL_l5Oqtg0FHPxI')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет')
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import telebot
-
-
-bot = telebot.TeleBot('6252209212:AAFW8AnZirrhTBi-4xveFL_l5Oqtg0FHPxI')
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Привет')
+from entitys.bot import bot
 
 
 if __name__ == '__main__':
